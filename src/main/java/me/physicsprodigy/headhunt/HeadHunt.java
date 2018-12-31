fe6939417a7ac80661c897c831014b69fc2b6e52
PhysicsProdigy <Wintonlim951@gmail.com>
2019-01-01 00:22:24 +1100

    Initial commit

---
+++ src/main/java/me/physicsprodigy/headhunt/HeadHunt.java
@@ -0,0 +1,17 @@
+package me.physicsprodigy.headhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HeadHunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
